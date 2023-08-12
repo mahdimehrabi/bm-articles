beb3689e16d28f7206677fa7e8d3b9a56065eba1
mahdi mehrabi <mahdimehrabi17@gmail.com>
2023-08-12 18:47:20 +0330

    add: user id to models

--- src/entities/Comment.go
+++ src/entities/Comment.go
@@ -1,6 +1,7 @@
 package entities
 
 type Comment struct {
+	ID       int64
 	UserID   int64
 	Fullname string
 	Body     string
--- src/entities/article.go
+++ src/entities/article.go
@@ -1,9 +1,11 @@
 package entities
 
 type Article struct {
+	ID       int64
 	Title    string
 	Body     string
 	Tags     string
 	Price    float64
 	BuyCount int64
+	UserID   int64
 }
